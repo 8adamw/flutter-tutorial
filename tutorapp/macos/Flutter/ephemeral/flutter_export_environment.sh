4474887003b3897e94d7b21f46f232ee987a826a
8adamw <104297172+8adamw@users.noreply.github.com>
2022-11-03 13:26:49 +0700

    redo init commit

--- tutorapp/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ tutorapp/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Fujitsu\Desktop\PBP_REPO\pbp-flutter-tutorial\tutorapp"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Fujitsu\Desktop\PBP_REPO\flutter-tutorial\tutorapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
